52f60efe0fc1ae0c3ba3df58b471983a02a1d82b
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-08-01 22:54:27 -0700

    Instead of destroying messages, recover them to an "Unknown" contact

--- src/mod.ts
+++ src/mod.ts
@@ -3,11 +3,16 @@ import type { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
 import type { IAkiProfile } from "@spt-aki/models/eft/profile/IAkiProfile";
 import type { StaticRouterModService } from "@spt-aki/services/mod/staticRouter/StaticRouterModService";
 import type { ProfileHelper } from "@spt-aki/helpers/ProfileHelper";
+import { MemberCategory } from "@spt-aki/models/enums/MemberCategory";
+import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 
 class Mod implements IPreAkiLoadMod
 {
+    logger: ILogger;
+
     public preAkiLoad(container: DependencyContainer): void 
     {
+        this.logger = container.resolve<ILogger>("WinstonLogger");
         const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
         const profileHelper = container.resolve<ProfileHelper>("ProfileHelper");
         
@@ -22,7 +27,7 @@ class Mod implements IPreAkiLoadMod
                         if (sessionId)
                         {
                             const fullProfile = profileHelper.getFullProfile(sessionId);
-                            this.checkForAndRemoveUndefinedDialogs(fullProfile);
+                            this.checkForAndRecoverUndefinedDialogs(fullProfile);
                         }
                         
                         return output;
@@ -34,14 +39,55 @@ class Mod implements IPreAkiLoadMod
     }
 
     /**
-     * Check for a dialog with the key 'undefined', and remove it
+     * Check for a dialog with the key 'undefined', and recover it by marking it as from 'Unknown'
      * @param fullProfile Profile to check for dialog in
      */
-    protected checkForAndRemoveUndefinedDialogs(fullProfile: IAkiProfile): void
+    protected checkForAndRecoverUndefinedDialogs(fullProfile: IAkiProfile): void
     {
         const undefinedDialog = fullProfile.dialogues["undefined"];
         if (undefinedDialog)
         {
+            this.logger.warning("Found undefined dialog, recovering...");
+            const unknownDialog = fullProfile.dialogues["Unknown"];
+            if (unknownDialog)
+            {
+                this.logger.info(`Found Unknown contact with ${unknownDialog.messages.length} messages, merging undefined with ${undefinedDialog.messages.length} messages...`);
+
+                // If the profile already has an "unknown" sender, prepend the messages
+                unknownDialog.messages = undefinedDialog.messages.concat(unknownDialog.messages);
+                unknownDialog.new = undefinedDialog.new;
+            }
+            else
+            {
+                this.logger.info(`Creating Unknown contact with ${undefinedDialog.messages.length} messages`);
+
+                // Otherwise just set the whole Unknown dialogues object, updating some properties as required
+                undefinedDialog._id = "Unknown";
+                undefinedDialog.type = 1;
+
+                // Setup the user if required
+                if (!undefinedDialog.Users)
+                {
+                    undefinedDialog.Users = [{
+                        _id: "Unknown",
+                        info: {
+                            Level: 0,
+                            Nickname: "Unknown",
+                            Side: "Usec",
+                            MemberCategory: MemberCategory.SYSTEM
+                        }
+                    }];
+                }
+
+                fullProfile.dialogues["Unknown"] = undefinedDialog;
+            }
+
+            // Update the _id of the messages as well, otherwise we get an exception
+            for (const message of fullProfile.dialogues["Unknown"].messages)
+            {
+                message.uid = "Unknown";
+            }
+
             delete fullProfile.dialogues["undefined"];
         }
     }
